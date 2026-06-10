117a87a07200fa3df064c30967d38ff7f75208bf
Radek Briza <radek.briza@post.cz>
2026-06-10 22:24:47 +0200

    Pridan ovladac Oled display

--- App/DisplayModule.cpp
+++ App/DisplayModule.cpp
@@ -1,13 +1,49 @@
+#include "main.h"
 #include "FreeRTOS.h" // IWYU pragma: keep.
 #include "task.h"
 #include "Message.hpp"
 #include <cstdio>
+#include "SSD1306_OLED.hpp"
+
+#define myOLEDwidth  128
+#define myOLEDheight 64
+#define FULLSCREEN (myOLEDwidth * (myOLEDheight/8))
+SSD1306 myOLED(myOLEDwidth ,myOLEDheight) ; // instantiate a OLED object
+
 
 [[maybe_unused]] 
 void DisplayTask(void* argument){ 
    bool CommunicationError =false;
     Message msg;
     
+    /* oled initialization */
+    uint8_t  screenBuffer[FULLSCREEN];
+    const uint8_t I2C_Address = 0x3C;
+    bool I2C_debug = false;
+    printf("OLED Test Begin\r\n");
+    // Turn on I2C bus (optionally it may already be on)
+    if(!myOLED.OLED_I2C_ON()){
+	    printf("Error 1202: bcm2835_i2c_begin :Cannot start I2C, Running as root?\n");
+	
+    }
+    else {
+        printf("SSD1306 library Version Number :: %u\r\n",myOLED.getLibVerNum());
+        HAL_Delay(500);
+        myOLED.OLEDbegin( I2C_Address, I2C_debug); // initialize the OLED
+        myOLED.OLEDFillScreen(0xF0, 0); // splash screen bars, optional just for effect
+        if (!myOLED.OLEDSetBufferPtr(
+            myOLEDwidth, myOLEDheight,
+             screenBuffer,
+              sizeof(screenBuffer))) return;
+        myOLED.OLEDclearBuffer();
+        myOLED.setTextColor(WHITE);
+        myOLED.setCursor(10, 10);
+        myOLED.print("Hello World.");
+        myOLED.OLEDupdate();
+    }
+
+
+
     while (true){  
         auto ok= xQueueReceive(
                 QueueDisplay,
